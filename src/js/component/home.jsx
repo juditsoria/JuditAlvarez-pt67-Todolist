41832f3fd538eaf96ba0cb61a68a3e1e574347d8
juditsoria <160534450+juditsoria@users.noreply.github.com>
2024-05-27 20:03:28 +0000

    first

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,46 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [input, setInput] = useState("");
+  const [tasklist, setTaskList] = useState(["jorge", "judit"]);
+
+  const text = (event) => {
+    setInput(event.target.value);
+  };
+
+  const addTask = () => {
+    if (input.trim()) {
+      setTaskList([...tasklist, input]);
+      setInput("");
+    }
+  };
+
+  const handleDelete = (index) => {
+		const newArr = []
+		for (let i = 0; i < taskLitss.length; i++) {
+			if (i !== index) {
+				newArr.push(taskLitss[i])
+			}
+		}
+		console.log(newArr)
+		setTaskLitss(newArr)
+	}
+
+  return (
+    <div>
+      <input type="text" value={input} onChange={text} />
+      <button onClick={addTask}>Add Task</button>
+      <ul>
+        {tasklist.map((item, index) => (
+          <React.Fragment key={index}>
+            <button onClick={() => handleDelete(index)}>{item}x</button>
+            <li>{item}</li>
+          </React.Fragment>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
